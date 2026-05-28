f709eca05633223b257fa73dcf1f36fbd0c80138
Chepuri Manasa <manasa12.chepuri@gmail.com>
2026-05-28 13:52:36 +0530

    Added Toggle and RandomUser

--- WebProject/src/main/resources/static/js/toggle.js
+++ WebProject/src/main/resources/static/js/toggle.js
@@ -1,17 +1,23 @@
-console.log("Hello from JS");
-
 var users = [
-    {"name" : "John", "gender" : "Male", "img" : "img/john.png"},
-    {"name" : "Jane", "gender" : "Female", "img" : "img/jane.png"}
+    {
+        name: "John",
+        gender: "Male",
+        img: "img/john.png"
+    },
+    {
+        name: "Jane",
+        gender: "Female",
+        img: "img/jane.png"
+    }
 ];
 
 var currentUserIndex = 0;
 
-function toggleCard(){
-    currentUserIndex =(currentUserIndex + 1) % 2;
+function toggleUser() {
+    currentUserIndex = (currentUserIndex + 1) % users.length;
+
     var user = users[currentUserIndex];
 
-    // DOM Manipulation
     document.getElementById("userImage").src = user.img;
     document.getElementById("userName").innerHTML = user.name;
     document.getElementById("userGender").innerHTML = user.gender;
