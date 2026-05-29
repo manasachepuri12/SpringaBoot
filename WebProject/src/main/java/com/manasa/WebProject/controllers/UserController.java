2f988d26b65fcd673d81180c55ee52397f4f2982
Chepuri Manasa <manasa12.chepuri@gmail.com>
2026-05-29 21:59:11 +0530

    Added Service and Controller files

--- WebProject/src/main/java/com/manasa/WebProject/controllers/UserController.java
+++ WebProject/src/main/java/com/manasa/WebProject/controllers/UserController.java
@@ -1,5 +1,25 @@
 package com.manasa.WebProject.controllers;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.manasa.WebProject.model.User;
+import com.manasa.WebProject.services.UserService;
+
+@RestController
+@RequestMapping("/api/users")
 public class UserController {
-    
+
+    private UserService userService;
+    public UserController() {
+        userService = new UserService();
+    }
+    @GetMapping
+    public ResponseEntity<List<User>> getAll() {
+        return ResponseEntity.ok(userService.getAllUsers());
+    }
 }
--- WebProject/src/main/java/com/manasa/WebProject/services/UserService.java
+++ WebProject/src/main/java/com/manasa/WebProject/services/UserService.java
@@ -1,5 +1,21 @@
 package com.manasa.WebProject.services;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.manasa.WebProject.model.User;
+
 public class UserService {
-    
+
+    private List<User> allUsers;
+
+    public UserService() {
+        allUsers = new ArrayList<>();
+        allUsers.add(new User("John", "Male", "img/john.png", 0));
+        allUsers.add(new User("Jane", "Female", "img/jane.png", 1));
+    }
+
+    public List<User> getAllUsers() {
+        return allUsers;
+    }
 }
